b6f27b863a06547d0aa0ef5f10fe941b4a9ee9c4
ST10448032 <ST10448032@rcconnect.edu.za>
2025-09-06 00:47:24 +0000

    Add files via upload

---
+++ MainWindow.xaml.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Monthly_Claims_System
+{
+    public partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitClaim_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LecturerSubmitPage());
+        }
+
+        private void UploadDocuments_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new UploadDocumentsPage()); // ✅ now a Page
+        }
+
+        private void TrackStatus_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new TrackStatusPage()); // ✅ now a Page
+        }
+
+        private void VerifyClaims_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new VerifyApprovePage()); // ✅ now a Page
+        }
+    }
+}
+
