63910e4b717218c221d63c9014a13a983dd498e1
ST10448032 <ST10448032@rcconnect.edu.za>
2025-09-06 00:49:29 +0000

    Add files via upload

---
+++ VerifyApproveVeiw.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Monthly_Claims_System
+{
+    public partial class VerifyApprovePage : Page
+    {
+        public VerifyApprovePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Verify_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Claim has been verified successfully!", "Verification",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Approve_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Claim has been approved!", "Approval",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Reject_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Claim has been rejected!", "Rejection",
+                MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
